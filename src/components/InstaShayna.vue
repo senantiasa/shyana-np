199cf71922fb1a6149653d19f932cc4edaeb2bc5
senantiasa <aldikurniawanfn@gmail.com>
2021-02-28 10:03:24 +0700

    upload project vue

---
+++ src/components/InstaShayna.vue
@@ -0,0 +1,48 @@
+<template>
+    <!-- Instagram Section Begin -->
+    <div class="instagram-photo">
+        <div class="insta-item set-bg" data-setbg="img/insta-1.jpg" style="background-image: url('/img/insta-7.jpg')">
+            <div class="inside-text">
+                <i class="ti-instagram"></i>
+                <h5><a href="https://www.instagram.com/gitasav/">gitasav</a></h5>
+            </div>
+        </div>
+        <div class="insta-item set-bg" style="background-image: url('/img/insta-9.jpg')">
+            <div class="inside-text">
+                <i class="ti-instagram"></i>
+                <h5><a href="https://www.instagram.com/kayla.nadira/">kayla.nadira</a></h5>
+            </div>
+        </div>
+        <div class="insta-item set-bg" style="background-image: url('/img/insta-8.jpg')">
+            <div class="inside-text">
+                <i class="ti-instagram"></i>
+                <h5><a href="https://www.instagram.com/aldikurniawanpanggabean/">aldikurniawanpanggabean</a></h5>
+            </div>
+        </div>
+        <div class="insta-item set-bg" style="background-image: url('/img/insta-10.jpg')">
+            <div class="inside-text">
+                <i class="ti-instagram"></i>
+                <h5><a href="https://www.instagram.com/pp.nikmahindah/">pp.nikmahindah</a></h5>
+            </div>
+        </div>
+        <div class="insta-item set-bg" style="background-image: url('/img/insta-11.jpg')">
+            <div class="inside-text">
+                <i class="ti-instagram"></i>
+                <h5><a href="https://www.instagram.com/anggimarito/">anggimarito/</a></h5>
+            </div>
+        </div>
+        <div class="insta-item set-bg" style="background-image: url('/img/insta-12.jpg')">
+            <div class="inside-text">
+                <i class="ti-instagram"></i>
+                <h5><a href="https://www.instagram.com/sufumalinda/">sufumalinda</a></h5>
+            </div>
+        </div>
+    </div>
+    <!-- Instagram Section End -->
+</template>
+
+<script>
+export default {
+    name: "InstaShayna"
+}
+</script>
